185fcae1d93d02748bdf68d8a237bd3dc28efd9c
vedhikaa25 <vedhikaa25@gmail.com>
2025-10-05 22:41:54 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,30 @@ app.get('/', (req, res) =>{
     res.json({msg:"hello students!"})
 });
 
+// CRUD functionalities of a movie 
+
+//R- for Reading the movies
+app.get('/movies',() =>{
+
+});
+
+// C- for Creating a movie
+app.post('/movies',() =>{
+
+});
+
+
+// U- for Updating a movie
+app.put('/movies/:id',() =>{
+
+});
+
+app.delete('/movies/:id',() =>{
+
+});
+
+
+// D - for Deleting movie
 app.listen(PORT,() =>{
     console.log(`The server is running at http://localhost:${PORT}`)
 });    
